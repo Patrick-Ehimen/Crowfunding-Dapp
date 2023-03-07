cad3f8a4d733dde08db79bf261e6b9ec493ce1b1
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-03-07 12:54:45 +0100

    Update hardhat.config.js

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,7 +1,15 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: '0.8.9',
+    version: "0.8.9",
+    defaultNetwork: "mumbai",
+    networks: {
+      hardhat: {},
+      mumbai: {
+        url: "https://polygon-mumbai.g.alchemy.com/v2/gi-O03E4Rsv0aEUJ3Ta4v4BP5Ik6jVWu",
+        accounts: ["0x${process.env.PRIVATE_KEY}"],
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
